a92da10a65483ebdf0bb129cdb8794d769ddc7f8
Jason Spicher <spicherj340@gmail.com>
2026-02-11 19:05:17 -0500

    mod_02 done

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,11 +10,11 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
-        max_depth=8,
-        subsample=1,
-        min_samples_leaf=1,
+        learning_rate=0.2,
+        n_estimators=5,
+        max_depth=6,
+        subsample=.9,
+        min_samples_leaf=30,
         random_state=seed
     )
     model.fit(X, y)
